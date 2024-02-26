48b9544c067b702b3b03208ca4d2a35786daba8b
AnnaHyttinen <93588412+AnnaHyttinen@users.noreply.github.com>
2024-02-26 13:46:21 +0200

    Create a day got a sensible user input too

--- SimpleTripPlanner.cpp
+++ SimpleTripPlanner.cpp
@@ -5,9 +5,14 @@
 using namespace std;
 
 void CreateDay() {
-    char* name = new char[10];
-    cout << "Type a preferred name for the day:\n";
-    cin >> name;
+    char* name = new char[20];
+    cout << "Type a preferred name for the day: ";
+    while (cin) {
+        cin.getline(name, sizeof(name));
+        if (cin.peek() == '\n') {
+            cin.clear(ios::eofbit);
+        }
+    }
     Day day(name);
     day.GetName();
     day.SwitchAction();
@@ -17,21 +22,6 @@ int main()
 {
     CreateDay();
     
-    /*
-    String toDo1 = "Swim with the dolphins.";
-    cout << toDo1 << endl;
-    cout << &toDo1 << endl; //the String pointer
-    
-    String toDo2 = "Run in the flower fields.";
-    String toDo3 = "Have a cake.";
-
-    LList<String*> toDoList1;
-    toDoList1.insertToEnd(&toDo1).insertToEnd(&toDo2).insertToEnd(&toDo3);
-    toDoList1.Print();
-
-    String hotel1 = "Oceanside hotel";
-    cout << hotel1 << endl;
-    */
     return 0;
 }
 
